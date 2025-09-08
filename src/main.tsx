a16f14fdf3a1a3d2fc50043aa3fe95f94d43c95b
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-09-08 19:04:06 +0530

    remove  theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ThemeProvider } from "./components/them-provider.tsx";
+
 import { BrowserRouter } from "react-router";
 import { Toaster } from "./components/ui/sonner.tsx";
 import { ContextProvider } from "./context/user-content.tsx";
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <ContextProvider>
-      <ThemeProvider>
-        <App />
-        <Toaster />
-      </ThemeProvider>
+      <App />
+      <Toaster />
     </ContextProvider>
   </BrowserRouter>
 );
